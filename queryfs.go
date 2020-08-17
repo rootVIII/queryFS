be3880bb05a3cad3a0c76d5e9f1d12a39faba49d
rootVIII <colleyloyejames@gmail.com>
2020-08-17 01:26:06 -0400

    add owner:group option

--- queryfs.go
+++ queryfs.go
@@ -9,15 +9,22 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
 	"strings"
+	"sync"
+	"syscall"
 )
 
 // QueryFS stores details related to the current search.
 type QueryFS struct {
-	Substring   string
-	Permissions string
-	IsTerm      bool
-	IsPerm      bool
+	Substring    string
+	Permissions  string
+	OwnerGroup   string
+	IsTerm       bool
+	IsPerm       bool
+	IsOwnerGroup bool
+	Group        map[int]string
+	Passwd       map[int]string
 }
 
 func (q QueryFS) processed(fileName string, processedDirectories []string) bool {
@@ -34,7 +41,13 @@ func (q QueryFS) processed(fileName string, processedDirectories []string) bool 
 func (q QueryFS) Query(path string, dirs []string) {
 	files, _ := ioutil.ReadDir(path)
 	for _, f := range files {
-		newPath := fmt.Sprintf("%s/%s", path, f.Name())
+		var newPath string
+		if path != "/" {
+			newPath = fmt.Sprintf("%s/%s", path, f.Name())
+		} else {
+			newPath = fmt.Sprintf("%s%s", path, f.Name())
+		}
+
 		if f.IsDir() {
 			if !q.processed(newPath, dirs) {
 				q.evaluate(newPath)
@@ -59,8 +72,57 @@ func (q QueryFS) evaluate(path string) {
 			goto end
 		}
 	}
+	if q.IsOwnerGroup {
+		fstat, err := os.Stat(path)
+		if err != nil {
+			goto end
+		}
+		if stat, ok := fstat.Sys().(*syscall.Stat_t); ok {
+			og := fmt.Sprintf("%s:%s", q.Passwd[int(stat.Uid)], q.Group[int(stat.Gid)])
+			if q.OwnerGroup != og {
+				goto end
+			}
+		}
+	}
 	fmt.Printf("%s\n", path)
 end:
+}
+
+func (q *QueryFS) parseIDS(idFile string, wgroup *sync.WaitGroup) {
+	defer wgroup.Done()
+	for _, line := range strings.Split(readIn(idFile), "\n") {
+		if len(line) > 0 {
+			lineItems := strings.Split(line, ":")
+			lineNo, _ := strconv.Atoi(lineItems[2])
+			if idFile[len(idFile)-6:] != "passwd" {
+				q.Group[int(lineNo)] = lineItems[0]
+			} else {
+				q.Passwd[int(lineNo)] = lineItems[0]
+			}
+		}
+	}
+}
+
+// SetIDS sets contents of /etc/passwd and /etc/group if querying with owner:group.
+func (q *QueryFS) SetIDS() {
+	if q.IsOwnerGroup {
+		q.Group = make(map[int]string)
+		q.Passwd = make(map[int]string)
+
+		var wg sync.WaitGroup
+		wg.Add(2)
+		go q.parseIDS("/etc/group", &wg)
+		go q.parseIDS("/etc/passwd", &wg)
+		wg.Wait()
+	}
+}
+
+func readIn(absPath string) string {
+	file, err := ioutil.ReadFile(absPath)
+	if err != nil {
+		displayError(err)
+	}
+	return string(file)
 }
 
 func displayError(e error) {
@@ -71,6 +133,7 @@ func displayError(e error) {
 func main() {
 	path := flag.String("d", "", "directory")
 	term := flag.String("s", "", "substring")
+	og := flag.String("o", "", "owner:group")
 	permissions := flag.String("p", "", "permissions")
 	flag.Parse()
 	if len(os.Args) < 5 {
@@ -91,10 +154,13 @@ func main() {
 	}
 
 	var qfs = &QueryFS{
-		Substring:   *term,
-		Permissions: *permissions,
-		IsTerm:      len(*term) > 0,
-		IsPerm:      len(*permissions) > 0,
+		Substring:    *term,
+		Permissions:  *permissions,
+		OwnerGroup:   *og,
+		IsTerm:       len(*term) > 0,
+		IsPerm:       len(*permissions) > 0,
+		IsOwnerGroup: len(*og) > 0,
 	}
+	qfs.SetIDS()
 	qfs.Query(start, []string{})
 }
